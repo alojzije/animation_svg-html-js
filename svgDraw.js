d1d984f7f6d98ed0f6ef9e9acdf210efd3866b46
alojzije <shljunak@hotmail.com>
2014-06-02 02:52:58 +0200

    Add come changes to getMover() and animateMover(), now multiple objects can be animated

--- svgDraw.js
+++ svgDraw.js
@@ -1,41 +1,45 @@
 
 
-function getMover(){
-    var pos = new Vector2D(0, $(window).height()-10);
-    var veloc = new Vector2D(9,-13);
+function getMover(id,x,y){
+    var xCoord = isFinite(x) ? x: 0;
+    var yCoord = isFinite(y) ? y: 0;
+    var pos = new Vector2D(xCoord, yCoord);
+    var veloc = new Vector2D(9,-12);
     var acc = new Vector2D(0,0);
     //m = new Mover('#ellipse1', pos, veloc, acc, 2);
     //m = new Rect('#rect1', pos, veloc, acc, 2);
 
-    var m = new Mover('#circle1', pos, veloc, acc, 2);
+    var m = new Mover(id, pos, veloc, acc, 2);
 
 
     return m;
  }
 
 function animateMover(m){
-	var gravity = new Vector2D(0,0.1*m.mass);
+    var gravity = new Vector2D(0,0.1*m.mass);
     //wind = new Vector2D(0.04,0);
-    var circleIntervalId = window.setInterval( function(){
+    var intervalId = window.setInterval( function(){
         friction = Vector2D.getNormalized(m.velocity)
-        friction.mult(-0.05)
+        friction.mult(-0.03)
          m.applyForce(friction);
          m.applyForce(gravity);
          //m.applyForce(wind);
          m.update()
          m.show()
          m.checkEdges()
-         m.checkIfFinished(circleIntervalId)
+         m.checkIfFinished(intervalId)
         
     }, 10);
 
 }
 
 $(document).ready(function() {
-	var mover = getMover();
-	 animateMover(mover);
-	console.log(mover.position);
-	// drawFractal
+    var mover = getMover('#circle1', 0, $(window).height()-10);
+     animateMover(mover);
+     animateMover(getMover('#circle2'));
+
+    console.log(mover.position);
+    // drawFractal
 
 });
 
